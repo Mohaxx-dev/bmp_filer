66961d49e2a64d4831abd352a5d74be7e9c57271
Bouderbala Mohammed Amine <bouderbalamohammed2000@gmail.com>
2020-08-11 16:30:40 +0100

    remove class

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,9 @@
 import cv2
-from bmpFiler import bmpFiler
+import numpy as np
+import bmpFiler.bmpFiler as bf
 
 if "__main__" == __name__:
     img = cv2.imread("kala.jpg")
     #TO BGRA
-    img = np.insert(img, 3, 0, axis=2)
-    bmpFiler.make_file(img, color_mode=bmpFiler.COLOR_MODE_BGRA, name="converted_file")
+    img = np.insert(img, 3, 255, 2)
+    bf.make_file(img, color_mode=bf.COLOR_MODE_BGRA, name="converted_file")
